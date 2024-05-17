d86a1af325e314d05a4dd07a9fd3b9fe15e9d8c5
TardivoJP <tardivojoaopedro@gmail.com>
2024-05-16 23:42:44 -0300

    Change to class names

--- LinkedList/LC143ReorderList.java
+++ LinkedList/LC143ReorderList.java
@@ -1,4 +1,4 @@
-public class LC143ReorderList {
+class ReorderList {
     public static void main(String[] args){
         ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
         ListNode list2 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4,  new ListNode(5)))));
--- LinkedList/LC19RemoveNthNodeFromEndOfList.java
+++ LinkedList/LC19RemoveNthNodeFromEndOfList.java
@@ -1,4 +1,4 @@
-class LC19RemoveNthNodeFromEndOfList {
+class RemoveNthNodeFromEndOfList {
     public static void main(String[] args){
         ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4,  new ListNode(5)))));
         ListNode list2 = new ListNode(1);
